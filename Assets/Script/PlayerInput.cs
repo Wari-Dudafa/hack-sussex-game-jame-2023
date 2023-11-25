9cbf445bc0bc18c61138e0dc65dcc07bdfee67bf
cactus <jacopo.bob.calvi@gmail.com>
2023-11-25 15:09:38 +0000

    Player Input

---
+++ Assets/Script/PlayerInput.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private List<KeyCode> availableKeys;
+    private int keysLeft;
+
+    public Dictionary<KeyCode, int> currentKeys;
+
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+    public KeyCode fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        availableKeys = new List<KeyCode>()
+        {
+            KeyCode.Q,
+            KeyCode.E,
+            KeyCode.R,
+            KeyCode.T,
+            KeyCode.Y,
+            KeyCode.U,
+            KeyCode.I,
+            KeyCode.O,
+            KeyCode.P,
+            KeyCode.F,
+            KeyCode.G,
+            KeyCode.H,
+        };
+
+        keysLeft = availableKeys.Count;
+
+        up = KeyCode.W;
+        down = KeyCode.S;
+        left = KeyCode.A;
+        right = KeyCode.D;
+        fire = KeyCode.Mouse0;
+
+        currentKeys = new Dictionary<KeyCode, int>()
+        {
+            { up, 0 },
+            { down, 0 },
+            { left, 0 },
+            { right, 0 },
+            { fire, 0 },
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        if (Input.GetKeyDown(up))
+        {
+            up = handleInput(up);
+        }
+
+        if (Input.GetKeyDown(down))
+        {
+            down = handleInput(down);
+        }
+
+        if (Input.GetKeyDown(left))
+        {
+            left = handleInput(left);
+        }
+
+        if (Input.GetKeyDown(right))
+        {
+            right = handleInput(right);
+        }
+
+        if (Input.GetKeyDown(fire))
+        {
+            fire = handleInput(fire);
+        }
+    }
+
+    KeyCode handleInput(KeyCode key)
+    {
+
+        Debug.Log("Key pressed" + key);
+        currentKeys[key] += 1;
+
+        if (currentKeys[key] > 2)
+        {
+            currentKeys.Remove(key);
+            keysLeft -= 1;
+
+            if (keysLeft <= 0)
+            {
+                key = KeyCode.None;
+                Debug.Log("No more keys available!");
+            }
+            else
+            {
+                key = availableKeys.ToArray<KeyCode>()[Random.Range(0, keysLeft - 1)];
+                availableKeys.Remove(key);
+
+                currentKeys.Add(key, 0);
+
+                Debug.Log("New key = " + key);
+            }
+        }
+        return key;
+    }
+}
