1d6fea50b31f9a3246aa1590ea9b6c9b7f5cc2f3
cactus <jacopo.bob.calvi@gmail.com>
2023-11-25 17:08:05 +0000

    Sprite direction added

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Transform gunTranform;
     public Transform playerTransform;
+    public Transform playerSpriteTransform;
     private Vector3 mousePos;
     private Vector3 objectPos;
     private float angle;
     public float speed;
+    public bool isFacingRight;
 
     void Update()
     {
@@ -25,7 +30,9 @@ public class PlayerMovement : MonoBehaviour
         mousePos.x = mousePos.x - objectPos.x;
         mousePos.y = mousePos.y - objectPos.y;
         angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        gunTranform.rotation = Quaternion.Euler(0, 0, angle);
+
+        updateSpriteDirection();
     }
 
     void MovePlayer()
@@ -35,5 +42,26 @@ public class PlayerMovement : MonoBehaviour
         Vector2 direction = new Vector2(horizontal, vertical) * speed * Time.deltaTime;
 
         playerTransform.Translate(direction, Space.World);
+    }
+
+    void FlipSprite()
+    {
+        Vector3 newScale = playerSpriteTransform.localScale;
+        newScale.x *= -1;
+        playerSpriteTransform.localScale = newScale;
+    }
+
+    void updateSpriteDirection()
+    {
+        if (mousePos.x < 0 && isFacingRight)
+        {
+            FlipSprite();
+            isFacingRight = false;
+        }
+        else if (mousePos.x >= 0 && !isFacingRight)
+        {
+            FlipSprite();
+            isFacingRight = true;
+        }
     }
 }
